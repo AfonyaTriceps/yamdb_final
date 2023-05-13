2583f1ef918eda294ed14642c6fe30a0828b5e02
Стас AfonyaTriceps <d.vlad957@gmail.com>
2023-05-13 20:29:03 +0300

    f

--- api_yamdb/api_yamdb/mixins.py
+++ api_yamdb/api_yamdb/mixins.py
@@ -2,9 +2,8 @@ from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.serializers import BaseSerializer
 
+from api_yamdb.permissions import IsAdminOwnerModerOrRdOnly
 from user.validators import validate_username
-
-from .permissions import IsAdminOwnerModerOrRdOnly
 
 
 class ListCreateDestroyViewSet(
